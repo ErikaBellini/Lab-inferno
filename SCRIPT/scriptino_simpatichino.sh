238a39a5a6ea2ba5571c6a38d59edede4e0a4308
Erika Bellini - erika.bellini@studio.unibo.it <STUDENTI\erika.bellini@sgbga-d142244sv.personale.dir.unibo.it>
2024-11-25 12:20:26 +0100

    Automated commit of scripts on 2024-11-25 12:20:26

---
+++ SCRIPT/scriptino_simpatichino.sh
@@ -0,0 +1,9 @@
+mkdir aprimi
+mkdir aprimi/apri_anche_me
+mkdir aprimi/apri_anche_me/continua
+mkdir aprimi/apri_anche_me/continua/prosegui
+mkdir aprimi/apri_anche_me/continua/prosegui/non_fermarti
+mkdir aprimi/apri_anche_me/continua/prosegui/non_fermarti/ANCORA
+mkdir aprimi/apri_anche_me/continua/prosegui/non_fermarti/ANCORA/CONTINUA_MALEDETTO
+touch aprimi/apri_anche_me/continua/prosegui/non_fermarti/ANCORA/CONTINUA_MALEDETTO/aprimi.txt
+echo "suca" > aprimi/apri_anche_me/continua/prosegui/non_fermarti/ANCORA/CONTINUA_MALEDETTO/aprimi.txt
